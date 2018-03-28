aa177b81d1aff0850028e77c888ebacaccc95ac4
unknown <smunyili@gmail.com>
2018-03-28 10:52:44 +0300

    add users

---
+++ chat.py
@@ -0,0 +1,18 @@
+
+import datetime
+from time import gmtime, strftime
+
+users = [
+    {
+        "id": 1, 'username': 'admin', 'password': 'admin', 'userType': 1, "lastLogin": None
+    },
+    {
+        "id": 2, 'username': 'sam', 'password': 'sam', 'userType': 3, "lastLogin": None
+    },
+    {
+        "id": 3, 'username': 'brian', 'password': 'brian', 'usetType': 2, "lastLogin": None
+    },
+    {
+        "id": 4, 'username': 'david', 'password': 'david', 'usetType': 3, "lastLogin": None
+    },
+]
