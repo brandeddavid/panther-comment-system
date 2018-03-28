19b9e431b1207f0dca5428ec9bbb5bfa468b3322
brian-mecha <brianmecha@gmail.com>
2018-03-28 11:18:47 +0300

    add parent on comments list

--- chat.py
+++ chat.py
@@ -3,12 +3,12 @@ import datetime
 # Comments list
 comments = [
     {"id":1, "commentMessage":"First message", "author":"Brian", "timeStamp":23-5-2018, "isParent": True},
-    {"id":1, "commentMessage":"Second message", "author":"David", "timeStamp":21-7-2018, "isParent": False}
+    {"id":2, "commentMessage":"Second message", "author":"David", "timeStamp":21-7-2018, "isParent": False, "parent":1}
 ]
 
 
 # Admin function
-def admin():
+def admin(user):
     print('What do you want to do? \n')
     print('Enter 1 for Edit comment\n')
     print('Enter 2 for Delete comment\n')
